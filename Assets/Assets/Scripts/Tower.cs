b5b4ce234aa4659b21abfb6ad9963f3a25e7d3c3
julianKim11 <juliankim0911@gmail.com>
2023-05-10 16:17:57 -0300

    0.45
    projectile, interfaz

--- Assets/Assets/Scripts/Tower.cs
+++ Assets/Assets/Scripts/Tower.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [SerializeField] private string projectileType;
     private SpriteRenderer mySpriteRenderer;
     private Enemy target;
-    
+    private bool canAttack = true;
+    private float attackTimer;
+    [SerializeField] private float attackCooldown;
     private Queue<Enemy> enemies = new Queue<Enemy>();
     private void Start()
     {
@@ -24,10 +27,33 @@ public class Tower : MonoBehaviour
     }
     private void Attack()
     {
+        if(!canAttack)
+        {
+            attackTimer += Time.deltaTime;
+
+            if(attackTimer >= attackCooldown)
+            {
+                canAttack = true;
+                attackTimer = 0;
+            }
+        }
         if(target == null && enemies.Count > 0)
         {
             target = enemies.Dequeue();
         }
+        if(target != null && target.IsActive)
+        {
+            if (canAttack)
+            {
+                Shoot();
+                canAttack = false;
+            }
+        }
+    }
+    private void Shoot()
+    {
+        Projectile projectile = GameManager.Instance.Pool.GetObject(projectileType).GetComponent<Projectile>();
+        projectile.transform.position = transform.position;
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
